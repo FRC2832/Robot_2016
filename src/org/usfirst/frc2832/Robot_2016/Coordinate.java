8ae5c7ba9e32613270876e45e36f4d302788fdc0
Zach Obsniuk <pi.guy314@yahoo.com>
2016-01-06 18:06:38 -0500

    Create Coordinate.java
    Seemed necessary for GamepadDeadzoned, so copied over.

---
+++ src/org/usfirst/frc2832/Robot_2016/Coordinate.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc2832.Robot_2015;
+
+public class Coordinate {
+	
+	public final double x, y;
+	
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
