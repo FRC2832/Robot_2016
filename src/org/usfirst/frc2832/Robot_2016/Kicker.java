b220a0c54525838edb5af30ab1cf4d8df9a382a9
SgtP4in <SgtP4in@gmail.com>
2016-01-22 20:57:29 -0500

    Merge

--- src/org/usfirst/frc2832/Robot_2016/Kicker.java
+++ src/org/usfirst/frc2832/Robot_2016/Kicker.java
@@ -11,23 +11,32 @@ public class Kicker extends Subsystem{
 		// TODO Auto-generated method stub
 		
 	}
-	
+	//push boulder out at a default angle
 	public static void launch()
 	{
 		double startAngle = RobotMap.kicker.getAngle();
 		RobotMap.kicker.setAngle(defaultLaunchAngle + startAngle);
-		
 	}
-	
+	//return after using default launch method
 	public static void resetAfterLaunch()
 	{
 		double currentAngle = RobotMap.kicker.getAngle();
 		RobotMap.kicker.setAngle(currentAngle - defaultLaunchAngle);
 	}
-	
+	//reset back to position acquired in robotInit()
 	public static void reset()
 	{
 		RobotMap.kicker.setAngle(Robot.defaultAngle);
+	}
+	
+	//accessor
+	public static double getAngle() {return RobotMap.kicker.getAngle();}
+	//accessor
+	public static void setAngle(double angle) {RobotMap.kicker.setAngle(angle);}
+	//simple method to move some angle
+	public static void moveAngle(double angle)
+	{
+		RobotMap.kicker.setAngle(RobotMap.kicker.getAngle() + angle);
 	}
 
 }
---
+++ src/org/usfirst/frc2832/Robot_2016/commands/Shoot.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc2832.Robot_2016.commands;
+
+import org.usfirst.frc2832.Robot_2016.Kicker;
+import edu.wpi.first.wpilibj.command.Command;
+
+/*
+ * Pushes out the ball assuming the ballmotors are already running
+ * Turns it off after timeout.
+ */
+public class Shoot extends Command {
+
+	private long timeStart;
+	//length of timeout, in milliseconds
+	private static final long TIMEOUT = 5000;
+	private static double startAngle;
+	private static final double ANGLE_TOLERANCE = 2; //how many degrees it wants to return within
+	
+	public Shoot()
+	{
+		//what does this require?
+		
+	}
+	
+	
+	protected void initialize() {
+		startAngle = Kicker.getAngle();
+		Kicker.launch();
+		//record time of command start
+		timeStart = System.currentTimeMillis();
+	}
+
+	@Override
+	protected void execute() {
+		Kicker.resetAfterLaunch();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		//returns true only when timeStart + TIMEOUT >= current time OR when the angle is within the TOLERANCE
+		return (Math.abs(Kicker.getAngle() - startAngle) < ANGLE_TOLERANCE || (timeStart + TIMEOUT) > System.currentTimeMillis());
+	}
+
+	@Override
+	protected void end() {
+		//if it didn't reach the tolerance (used the time-out maybe) then do the hard reset
+		if(Math.abs(Kicker.getAngle() - startAngle) > ANGLE_TOLERANCE)
+			Kicker.reset();
+
+	}
+
+	@Override
+	protected void interrupted() {
+		Kicker.reset(); //fail-safe
+	}
+
+}
