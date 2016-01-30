baef21d12aac84fcf0f48c3bf170f5b54e6036e3
John Rosner <johntherobot5000@gmail.com>
2016-01-30 10:23:44 -0500

    attempted Camera switching, not complete

--- src/org/usfirst/frc2832/Robot_2016/commands/InterfaceFlip.java
+++ src/org/usfirst/frc2832/Robot_2016/commands/InterfaceFlip.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc2832.Robot_2016.commands;
 
-import org.usfirst.frc2832.Robot_2016.RobotMap;
+import org.usfirst.frc2832.Robot_2016.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 /**
@@ -11,18 +11,23 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class InterfaceFlip extends Command {
 	
-	public static boolean isFlipped = true;
+	public static boolean isFlipped;
 
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
-		isFlipped = !isFlipped;
+		if (isFlipped == false) {
+			Robot.cameraServer.startAutomaticCapture(Robot.camera2);
+			isFlipped = true;
+		} else {
+			Robot.cameraServer.startAutomaticCapture(Robot.camera1);
+			isFlipped = true;
+		}
 	}
 
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
