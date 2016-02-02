e9d5f4b72473a3650f2d75305dc7a46e87c479cc
John Rosner <johntherobot5000@gmail.com>
2016-02-01 20:34:02 -0500

    Open cameras before reading

--- src/org/usfirst/frc2832/Robot_2016/DualCameraRunnable.java
+++ src/org/usfirst/frc2832/Robot_2016/DualCameraRunnable.java
@@ -5,11 +5,16 @@ import org.usfirst.frc2832.Robot_2016.commands.InterfaceFlip;
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.Image;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class DualCameraRunnable implements Runnable {
 
 	@Override
 	public void run() {
 		Image frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+		
+		Robot.camera1.openCamera();
+		Robot.camera2.openCamera();
 		
 		while (true) {
 			try {
@@ -20,7 +25,7 @@ public class DualCameraRunnable implements Runnable {
 				}
 				
 				Robot.cameraServer.setImage(frame);
-				
+				SmartDashboard.putNumber("camera test", Math.random());
 				
 				Thread.sleep(67);
 			} catch (InterruptedException e) {
