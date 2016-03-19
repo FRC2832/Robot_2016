3146c86058e8869a155b6090617f76f441a0919f
James Jahns <jjahns3885@gmail.com>
2016-03-19 11:47:33 -0400

    better rotation function and res changed to 640x480

--- src/org/usfirst/frc2832/Robot_2016/commands/ImagingTest.java
+++ src/org/usfirst/frc2832/Robot_2016/commands/ImagingTest.java
@@ -20,12 +20,25 @@ public class ImagingTest extends Command {
 	protected void execute() {
 		ImageProcessing.process();
 		//RobotMap.driveTrain.arcadeDrive(0, -1.2*(ImageProcessing.targetOffset[0]+0.094));
+		RobotMap.driveTrain.arcadeDrive(0, rotate(ImageProcessing.targetOffset[0]+0.094));
 		SmartDashboard.putNumber("offset X", ImageProcessing.targetOffset[0]);
 		SmartDashboard.putNumber("offset Y", ImageProcessing.targetOffset[1]);
 		SmartDashboard.putNumber("target depth", ImageProcessing.depth);
 		SmartDashboard.putNumber("height", ImageProcessing.height);
 	}
-
+	private double rotate(double offset)
+	{
+		if(offset>0.5)
+			return -1;
+		if(offset<-0.5)
+			return 1;
+		if(offset>0)
+			return -1.6*offset-0.2;
+		if(offset<0)
+			return -1.6*offset+0.2;
+		return 0;
+	}		
+		
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
