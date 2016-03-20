5d708f9340697561fb4a222054739d21894cf8c0
James Jahns <jjahns3885@gmail.com>
2016-03-20 16:44:43 -0400

    Horizontal vision code merge! -Pressing B now runs ImagingTest -ImagingTest uses GRIP to determine angle, then turns w/ RotateAngle -TODO: RotateAngle overshoots, esp. at angles <5 degrees -ImageProcessing changed to reflect 640x480 resolution

--- src/org/usfirst/frc2832/Robot_2016/commands/ImagingTest.java
+++ src/org/usfirst/frc2832/Robot_2016/commands/ImagingTest.java
@@ -1,25 +1,24 @@
 package org.usfirst.frc2832.Robot_2016.commands;
 
 import org.usfirst.frc2832.Robot_2016.RobotMap;
+import org.usfirst.frc2832.Robot_2016.commands.autonomous.RotateAngle;
 import org.usfirst.frc2832.Robot_2016.vision.ImageProcessing;
 
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //DEBUG USE ONLY
 //DO NOT USE FOR ANY LEGITIMATE PROCESS
 //THANKS :)
 public class ImagingTest extends Command {
 
+	private static final int FOV_ANGLE=22;
 	@Override
 	protected void initialize() {
-
-	}
-
-	@Override
-	protected void execute() {
 		ImageProcessing.process();
-		RobotMap.driveTrain.arcadeDrive(0, ImageProcessing.targetOffset[0]);
+		Scheduler.getInstance().add(new RotateAngle(-ImageProcessing.targetOffset[0]*FOV_ANGLE));
+		SmartDashboard.putNumber("angle", -ImageProcessing.targetOffset[0]*FOV_ANGLE);
 		SmartDashboard.putNumber("offset X", ImageProcessing.targetOffset[0]);
 		SmartDashboard.putNumber("offset Y", ImageProcessing.targetOffset[1]);
 		SmartDashboard.putNumber("target depth", ImageProcessing.depth);
@@ -27,9 +26,13 @@ public class ImagingTest extends Command {
 	}
 
 	@Override
+	protected void execute() {
+	}
+
+	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
